8682e81d616ba3b54948fccc44d284c868b240ff
ahmad hamza <ahmadhamza19@gmail.com>
2015-01-02 20:15:53 +0530

    form value added to each scope

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -6,15 +6,16 @@ app.controller('signupController', function($scope) {
   $scope.invalidEmail = false;
   $scope.form = {}
   $scope.submitForm = function (data) {
-  	if($scope.signup_form.$valid){
+    console.log($scope.form.signup_form.$valid);
+  	if($scope.form.signup_form.$valid){
   		$scope.emailRequired = false;
 			$scope.invalidEmail = false;	
   	}else{
-  		$scope.signup_form.submitted = true;
-  		if ($scope.signup_form.email.$error.required){
+  		$scope.form.signup_form.submitted = true;
+  		if ($scope.form.signup_form.email.$error.required){
   			$scope.emailRequired = true;
   			$scope.invalidEmail = false;
-  		}else if($scope.signup_form.email.$invalid){
+  		}else if($scope.form.signup_form.email.$invalid){
   			$scope.invalidEmail = true;
   			$scope.emailRequired = false;
   		}	
